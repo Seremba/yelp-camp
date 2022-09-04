cacc0ed93215f3c66d4fe7a805764c75d1025909
Seremba <pserembae@gmail.com>
2022-09-04 19:04:34 +0300

    create route to test connection of database

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Databse Connected');
+});
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -10,6 +21,11 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/create-campground', async (req, res) => {
+    let camp = new Campground({title: 'Nansana Camp', description: 'The most perfect place'});
+    await camp.save();
+    res.send(camp);
+});
 
 app.listen(3000, () => {
     console.log('App Listening On Port 3000');
