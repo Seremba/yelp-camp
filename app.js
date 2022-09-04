edbf5fd8507a88f3d5a23b4d06923e500cd0e42e
Seremba <pserembae@gmail.com>
2022-09-04 18:20:51 +0300

    basic set up for the app

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+
+    res.render('home');
+});
+
+
+app.listen(3000, () => {
+    console.log('App Listening On Port 3000');
+});
