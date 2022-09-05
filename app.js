a97b4cb16edae5fc314ed1c183ab2bf33810aa71
Seremba <pserembae@gmail.com>
2022-09-05 12:32:02 +0300

    create route handlers to delete a campground

--- app.js
+++ app.js
@@ -58,6 +58,27 @@ app.put('/campgrounds/:id', async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`);
 });
 
+app.delete('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params;
+    await  Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(3000, () => {
     console.log('App Listening On Port 3000');
