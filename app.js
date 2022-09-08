ffc96a9fe5aca20f665d63217dac0bd538ee8ca6
Seremba <pserembae@gmail.com>
2022-09-08 19:41:28 +0300

    add and require Joi to for server-side validation

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
 const Campground = require('./models/campground');
 const  methodOverride = require('method-override');
 const ExpressError = require('./utils/ExpressError');
 const catchAsyncError = require('./utils/catchAsyncError');
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 const db = mongoose.connection
@@ -39,7 +41,21 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 app.post('/campgrounds', catchAsyncError(async (req, res, next) => {
-        if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+        // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+        const campgroundSchema = Joi.object({
+            campground: Joi.object({
+                title: Joi.string().required(),
+                image: Joi.string().required(),
+                price: Joi.number().required(),
+                location: Joi.string().required(),
+                description: Joi.string().required()
+            }).required()
+        });
+        const { error } = campgroundSchema.validate(req.body);
+        if(error){
+            const msg = error.details.map(el => el.message).join(',');
+            throw new ExpressError(msg, 400);
+        }
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`);
@@ -71,13 +87,14 @@ app.delete('/campgrounds/:id', catchAsyncError(async (req, res) => {
 
 
 app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404))
+    next(new ExpressError('Page Not Found', 404));
 })
 
 
 app.use((err, req, res, next) => {
-    const {statusCode = 500, message = 'Something went wrong'} = err;
-    res.status(statusCode).send(message);
+    const {statusCode = 500} = err;
+    if(!err.message) err.message = 'Something went wrong';
+    res.status(statusCode).render('error', {err});
 })
 
 
