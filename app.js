f520c46b75815148c3c183fe5f660c361e530521
Seremba <pserembae@gmail.com>
2022-09-12 17:36:15 +0300

    create a post route handler for reviews

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const Campground = require('./models/campground');
 const  methodOverride = require('method-override');
 const ExpressError = require('./utils/ExpressError');
 const catchAsyncError = require('./utils/catchAsyncError');
+const Review = require('./models/review')
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
@@ -51,7 +52,6 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 app.post('/campgrounds', validateCampground, catchAsyncError(async (req, res, next) => {
-        // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`);
@@ -81,6 +81,20 @@ app.delete('/campgrounds/:id', catchAsyncError(async (req, res) => {
     res.redirect('/campgrounds');
 }));
 
+app.post('/campgrounds/:id/reviews', catchAsyncError(async (req, res) => {
+    const {id} = req.params;
+    const campground = await Campground.findById(id);
+    // create a new review
+    const review = new Review(req.body.review);
+    // push the review to the campground
+    campground.reviews.push(review);
+    //save review
+    await review.save();
+    //save campground
+    await campground.save()
+    //redirect to the show page
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
