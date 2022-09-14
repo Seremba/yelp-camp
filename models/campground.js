a1596313faf00c0fe05db9f1497fccfd799efe15
Seremba <pserembae@gmail.com>
2022-09-14 15:00:10 +0300

    delete a campground with its reviews completely from the DB

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review')
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -15,5 +16,15 @@ const CampgroundSchema = new Schema({
     ]
 });
 
+CampgroundSchema.post('findOneAndDelete', async (doc) => {
+
+    if(doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+});
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
