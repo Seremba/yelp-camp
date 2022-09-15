38d1d80225b18efcf24f0323ac3012a1c0b58ca0
Seremba <pserembae@gmail.com>
2022-09-15 23:23:52 +0300

    improve user schema model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
+const passportLocalMongoose = require('passport-local-mongoose');
 
-userSchema = new Schema({
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
 
-})
-
-module.exports = mongoose.model('User', userSchema)
+userSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model('User', userSchema);
