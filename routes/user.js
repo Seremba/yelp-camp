febd2d5c8f8d16a2d4b6695923147169b5c7d506
Seremba <pserembae@gmail.com>
2022-09-16 00:21:55 +0300

    create a register route handler for users to submit the form

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
-
+const catchAsyncError = require('../utils/catchAsyncError');
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', (req, res) => {
-    res.send(req.body);
-});
+router.post('/register', catchAsyncError( async (req, res) => {
+    try{
+        const {username, email, password} = req.body;
+        const user = await new User({username, email});
+        const registeredUser = await User.register(user, password);
+        req.flash('success','Welcome To Yelp Camp');
+        res.redirect('/campgrounds');
+    }catch(e){
+        req.flash('error', e.message);
+        res.redirect('/register');
+    }
+}));
 
 module.exports = router;
