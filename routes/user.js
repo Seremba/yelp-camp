0ea7908a2e06d219177abc7b8904e7a13e566015
Seremba <pserembae@gmail.com>
2022-09-16 15:25:54 +0300

    add logic for user to redirect to the url he wanted to get to before logging in

--- routes/user.js
+++ routes/user.js
@@ -30,9 +30,11 @@ router.get('/login', (req, res) => {
     res.render('users/login')
 });
 
-router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), (req, res) => {
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true, keepSessionInfo: true }), (req, res) => {
     req.flash('success', 'Welcome back to YelpCamp');
-    res.redirect('/campgrounds');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 });
 
 router.get('/logout', (req, res, next) => {
