63a2f8252c1c6cd36715087f4045f8b6d153b5bb
Seremba <pserembae@gmail.com>
2022-09-16 10:47:54 +0300

    use req.user object to get user information

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn = (req, res, next) => {
+    // console.log(req.user)
     if(!req.isAuthenticated()){
         req.flash('error', 'Please login!');
         return res.redirect('/login');
