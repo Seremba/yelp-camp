e7200a07669babde915843e26bd4f3a00a1aa0bb
Seremba <pserembae@gmail.com>
2022-09-16 09:47:52 +0300

    add isloggIn middleware to allow only logged in users to do somethings

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,6 +4,7 @@ const catchAsyncError = require('../utils/catchAsyncError');
 const Campground = require('../models/campground');
 const ExpressError = require('../utils/ExpressError');
 const {campgroundSchema} = require('../Schemas');
+const {isLoggedIn} = require('../middleware')
 
 const validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body);
@@ -20,11 +21,11 @@ router.get('/', catchAsyncError(async (req, res) => {
     res.render('campgrounds/index', {campgrounds});
 }));
 
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
     res.render('campgrounds/new');
 });
 
-router.post('/', validateCampground, catchAsyncError(async (req, res, next) => {
+router.post('/', isLoggedIn, validateCampground, catchAsyncError(async (req, res, next) => {
         const campground = new Campground(req.body.campground);
         await campground.save();
         req.flash('success', 'Successfully Created a New Campground!')
@@ -41,7 +42,7 @@ router.get('/:id', catchAsyncError(async (req, res) => {
     res.render('campgrounds/show', {campground});
 }));
 
-router.get('/:id/edit', catchAsyncError(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, catchAsyncError(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     if(!campground){
@@ -58,7 +59,7 @@ router.put('/:id', validateCampground, catchAsyncError(async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-router.delete('/:id', catchAsyncError(async (req, res) => {
+router.delete('/:id', isLoggedIn, catchAsyncError(async (req, res) => {
     const {id} = req.params;
     await  Campground.findByIdAndDelete(id);
     req.flash('success', 'Successfully Deleted Campground!');
