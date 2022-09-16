ab3ba052a2c58849ffe729649baf67f747a607a9
Seremba <pserembae@gmail.com>
2022-09-16 15:28:24 +0300

    add the original url to the session

--- middleware.js
+++ middleware.js
@@ -1,6 +1,8 @@
 module.exports.isLoggedIn = (req, res, next) => {
     // console.log(req.user)
     if(!req.isAuthenticated()){
+        // console.log(req.originalUrl)
+        req.session.returnTo = req.originalUrl;
         req.flash('error', 'Please login!');
         return res.redirect('/login');
     }
