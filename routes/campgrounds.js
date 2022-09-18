18f39c976e2f74ec126f58e993bc28526582b4cd
Seremba <pserembae@gmail.com>
2022-09-18 03:02:52 +0300

    populate an author to a certain review

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -24,7 +24,12 @@ router.post('/', isLoggedIn, validateCampground, catchAsyncError(async (req, res
 
 router.get('/:id', catchAsyncError(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findById(id).populate('reviews').populate('author');
+    const campground = await Campground.findById(id).populate({
+        path:'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
     console.log(campground)
     if(!campground){
         req.flash('error', 'Cannot find that campground');
