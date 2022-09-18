bc3366b5684407c05df1141b1e1614aa7e316767
Seremba <pserembae@gmail.com>
2022-09-18 03:05:04 +0300

    create isReviewAuthor middleware

--- middleware.js
+++ middleware.js
@@ -1,4 +1,6 @@
 const Campground = require('./models/campground');
+const Review = require('./models/review');
+
 const {campgroundSchema,reviewSchema } = require('./Schemas');
 const ExpressError = require('./utils/ExpressError');
 
@@ -28,6 +30,16 @@ module.exports.isAuthor = async (req, res, next) => {
     const {id} = req.params;
     const campground = await Campground.findById(id);
     if(!campground.author.equals(req.user._id)){
+        req.flash('error', 'Sorry, you do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const {id, reviewId} = req.params;
+    const review = await Review.findById(reviewId);
+    if(!review.author.equals(req.user._id)){
         req.flash('error', 'Sorry, you do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
     }
