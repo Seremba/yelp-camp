5637248ece77c9a59fe570ddbd11d93e34542fe8
Seremba <pserembae@gmail.com>
2022-09-19 00:13:01 +0300

    group routes with the identical paths with router.route method

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,19 +5,20 @@ const catchAsyncError = require('../utils/catchAsyncError');
 const Campground = require('../models/campground');
 const {isLoggedIn, validateCampground, isAuthor} = require('../middleware')
 
-
-router.get('/', catchAsyncError(campgrounds.index));
+router.route('/')
+    .get(catchAsyncError(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsyncError(campgrounds.createCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.post('/', isLoggedIn, validateCampground, catchAsyncError(campgrounds.createCampground));
+router.route('/:id')
+    .get( catchAsyncError(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsyncError(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsyncError(campgrounds.deleteCampground));
 
-router.get('/:id', catchAsyncError(campgrounds.showCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsyncError(campgrounds.renderEditForm));
 
-router.put('/:id',  isLoggedIn, isAuthor, validateCampground, catchAsyncError(campgrounds.updateCampground));
 
-router.delete('/:id', isLoggedIn, isAuthor, catchAsyncError(campgrounds.deleteCampground));
 
 module.exports = router;
