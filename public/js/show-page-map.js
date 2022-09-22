680efb21df73f6ac2b8f2e7b6dd5dec5093208fd
Seremba <pserembae@gmail.com>
2022-09-22 14:32:03 +0300

    show marker on a campground location

--- public/js/show-page-map.js
+++ public/js/show-page-map.js
@@ -2,7 +2,11 @@ mapboxgl.accessToken = mapBoxToken;
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-74.5, 40],
-    zoom: 9,
+    center: campground.geometry.coordinates,
+    zoom: 10,
     projection: 'globe'
 });
+
+const marker = new mapboxgl.Marker()
+.setLngLat(campground.geometry.coordinates)
+.addTo(map);
