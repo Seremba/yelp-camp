9ee2149d67839d15dc4e1e68c694a0fe7e273d31
Seremba <pserembae@gmail.com>
2022-09-25 18:05:30 +0300

    create a campground nested virtual for properties.popUpMarkup link to the campground

--- models/campground.js
+++ models/campground.js
@@ -1,55 +1,61 @@
-const mongoose = require('mongoose');
-const Review = require('./review')
+const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
-
 const ImageSchema = new Schema({
-        url: String,
-        filename: String
+  url: String,
+  filename: String,
 });
 
-ImageSchema.virtual('thumbnail').get(function(){
-    return this.url.replace('/upload', '/upload/w_200')
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
 });
 
-const CampgroundSchema = new Schema({
+const opts = { toJSON: { virtuals: true } };
+const CampgroundSchema = new Schema(
+  {
     title: String,
     images: [ImageSchema],
     geometry: {
-        type: {
-          type: String,
-          enum: ['Point'],
-          required: true
-        },
-        coordinates: {
-          type: [Number],
-          required: true
-        }
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
     price: Number,
     description: String,
     location: String,
     author: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
     reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Review'
-        }
-    ]
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  opts
+);
+
+CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
+  return `<a href='/campgrounds/${this.id}'>${this.title}</a>`;
 });
 
-CampgroundSchema.post('findOneAndDelete', async (doc) => {
-
-    if(doc) {
-        await Review.deleteMany({
-            _id: {
-                $in: doc.reviews
-            }
-        })
-    }
+CampgroundSchema.post("findOneAndDelete", async (doc) => {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
-module.exports = mongoose.model('Campground', CampgroundSchema);
+module.exports = mongoose.model("Campground", CampgroundSchema);
