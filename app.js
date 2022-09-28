dbbcb436f81fa51e8b1b543c0558315ba55e5053
Seremba <pserembae@gmail.com>
2022-09-28 11:32:41 +0300

    sanitize our mongo database using express-mongo-sanitize

--- app.js
+++ app.js
@@ -1,51 +1,52 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
-const session = require('express-session')
-const flash = require('connect-flash');
-const ExpressError = require('./utils/ExpressError');
-const  methodOverride = require('method-override');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const User = require('./models/user')
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
+const ExpressError = require("./utils/ExpressError");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const mongoSanitize = require("express-mongo-sanitize");
 
-const userRoutes = require('./routes/user');
-const campgroundsRoutes = require('./routes/campgrounds');
-const reviewsRoutes = require('./routes/reviews');
+const userRoutes = require("./routes/user");
+const campgroundsRoutes = require("./routes/campgrounds");
+const reviewsRoutes = require("./routes/reviews");
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
-const db = mongoose.connection
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
+const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-    console.log('Databse Connected');
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Databse Connected");
 });
 
 const app = express();
 
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize());
 
 const sessionConfig = {
-    secret: 'thisshouldbeabettersecret',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
+  secret: "thisshouldbeabettersecret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
 };
 
 app.use(session(sessionConfig));
@@ -53,56 +54,38 @@ app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use( new LocalStrategy(User.authenticate()));
+passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
-app.get('/', (req, res) => {
-
-    res.render('home');
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
+app.use("/", userRoutes);
 
-app.use('/', userRoutes);
+app.use("/campgrounds", campgroundsRoutes); // campground routes, check in routes/campgrounds.js
 
-app.use('/campgrounds', campgroundsRoutes); // campground routes, check in routes/campgrounds.js
+app.use("/campgrounds/:id/reviews", reviewsRoutes); // review routes, check in routes/reviews.js
 
-app.use('/campgrounds/:id/reviews', reviewsRoutes); // review routes, check in routes/reviews.js
-
-
-app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404));
-})
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
 
 app.use((err, req, res, next) => {
-    const {statusCode = 500} = err;
-    if(!err.message) err.message = 'Something went wrong';
-    res.status(statusCode).render('error', {err});
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Something went wrong";
+  res.status(statusCode).render("error", { err });
+});
 
 app.listen(3000, () => {
-    console.log('App Listening On Port 3000');
+  console.log("App Listening On Port 3000");
 });
