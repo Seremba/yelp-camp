dedc721a518330dcb1baf6fb864746482c610c22
Seremba <pserembae@gmail.com>
2022-09-29 12:39:39 +0300

    create a script for showing and hiding passwords logic

---
+++ public/js/showPassword.js
@@ -0,0 +1,9 @@
+const passwordField = document.querySelector("#password");
+const eyeIcon = document.querySelector("#eye");
+
+eye.addEventListener("click", function () {
+  this.classList.toggle("fa-eye-slash");
+  const type =
+    passwordField.getAttribute("type") === "password" ? "text" : "password";
+  passwordField.setAttribute("type", type);
+});
