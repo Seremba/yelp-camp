2cff8ed217f770cbaf81d4ae04614ebe85fd844e
Seremba <pserembae@gmail.com>
2022-09-29 12:54:35 +0300

    add height and width to the image url

--- models/campground.js
+++ models/campground.js
@@ -8,7 +8,7 @@ const ImageSchema = new Schema({
 });
 
 ImageSchema.virtual("thumbnail").get(function () {
-  return this.url.replace("/upload", "/upload/w_200");
+  return this.url.replace("/upload", "/upload/h_402,w_604,");
 });
 
 const opts = { toJSON: { virtuals: true } };
