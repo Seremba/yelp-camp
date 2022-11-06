f385fd812f2f10140ea111a03adad5090bf56920
Seremba <pserembae@gmail.com>
2022-11-06 09:14:11 +0300

    update localhost

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const userRoutes = require("./routes/user");
 const campgroundsRoutes = require("./routes/campgrounds");
 const reviewsRoutes = require("./routes/reviews");
 
-const dbUrl = process.env.DB_ULR || "mongodb://localhost:27017/yelp-camp";
+const dbUrl = process.env.DB_ULR || "mongodb://127.0.0.1:27017/yelp-camp";
 
 mongoose.connect(dbUrl);
 const db = mongoose.connection;
